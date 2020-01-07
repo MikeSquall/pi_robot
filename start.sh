3111bfdb5eb9d5b79667974f36e1ab811b8461bc
mikearbib <arbib.michael@gmail.com>
2020-01-07 23:13:34 +0200

    Handle stop on bash script

--- start.sh
+++ start.sh
@@ -12,12 +12,28 @@ Usage: ./start --ip 192.168.1.42 --port 3000
 DEFAULT_IP="127.0.0.1"
 DEFAULT_PORT="5000"
 
+function ctrl_c() {
+        echo "** Stop server and webapp **"
+        kill -9 $SERVER_PID
+        kill -9 $WEBAPP_PID
+        exit 0
+}
+
 main(){
       if [ -z $SERVER_IP ]; then IP="$DEFAULT_IP"; else IP="$SERVER_IP"; fi
       if [ -z $SERVER_PORT ]; then PORT="$DEFAULT_PORT"; else PORT="$SERVER_PORT"; fi
 
+      # Start backend server and store pid
       python3 server/server.py $IP $PORT &
-      cd webapp && pwd && yarn install && yarn start
+      SERVER_PID=$!
+
+      # Start webapp and store pid
+      cd webapp && pwd && yarn install
+      yarn start &
+      WEBAPP_PID=$!
+
+      # trap ctrl-c and call ctrl_c()
+      trap ctrl_c INT
     }
 
 while [[ $# -gt 1 ]]; do
