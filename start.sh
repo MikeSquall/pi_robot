4c767758d3159e2f573f7a27b8ddabad5cd13530
mikearbib <arbib.michael@gmail.com>
2020-01-07 22:52:04 +0200

    Added bash script to start server and webapp

---
+++ start.sh
@@ -0,0 +1,44 @@
+#!/usr/bin/env bash
+
+USAGE="""
+This script assumes all dependencies have been installed.
+Usage: ./start --ip 192.168.1.42 --port 3000
+    -i --ip
+        The ip you want to start the server on (default 127.0.0.1).
+    -p --port
+        The port you want to start the server on (default 5000).
+"""
+
+DEFAULT_IP="127.0.0.1"
+DEFAULT_PORT="5000"
+
+main(){
+      if [ -z $SERVER_IP ]; then IP="$DEFAULT_IP"; else IP="$SERVER_IP"; fi
+      if [ -z $SERVER_PORT ]; then PORT="$DEFAULT_PORT"; else PORT="$SERVER_PORT"; fi
+
+      python3 server/server.py $IP $PORT &
+      cd webapp && pwd && yarn install && yarn start
+    }
+
+while [[ $# -gt 1 ]]; do
+
+    arg="$1"
+    value="$2"
+    case $arg in
+        -i|--ip)
+            SERVER_IP="$value"
+            shift
+            ;;
+        -p|--port)
+            SERVER_PORT="$value"
+            shift
+            ;;
+        *)
+            echo "Invalid argument $arg."
+            return 1
+            ;;
+    esac
+    shift
+done
+
+main "$@"
